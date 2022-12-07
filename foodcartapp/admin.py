ca53c3ca0bb836ac8e94ec19aaadd87aaa0140ae
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-12-07 14:03:28 +0100

    registered Order, ProductQuantity

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -7,6 +7,7 @@ from .models import Product
 from .models import ProductCategory
 from .models import Restaurant
 from .models import RestaurantMenuItem
+from .models import Order, ProductQuantity
 
 
 class RestaurantMenuItemInline(admin.TabularInline):
@@ -104,3 +105,13 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(ProductCategory)
 class ProductAdmin(admin.ModelAdmin):
     pass
+
+
+class ProductQuantityInline(admin.TabularInline):
+    model = ProductQuantity
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [ProductQuantityInline,]
