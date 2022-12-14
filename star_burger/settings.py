78231df950a3596cac962b3865732ec32da021b3
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-12-14 18:13:33 +0100

    fixed ALLOWED_HOSTS

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', True)
 YANDEX_API_KEY = env('YANDEX_API_KEY')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
